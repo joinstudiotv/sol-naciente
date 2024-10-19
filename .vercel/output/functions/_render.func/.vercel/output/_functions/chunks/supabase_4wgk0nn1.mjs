93b89f0f8fbb9aadcfc811dc768d6af7b99630b6
Alan Monroy Bernedo <alan.monroy@ucsp.edu.pe>
2024-10-19 01:20:01 -0500

    test

--- .vercel/output/functions/_render.func/.vercel/output/_functions/chunks/supabase_4wgk0nn1.mjs
+++
@@ -1,8 +0,0 @@
-import { createClient } from '@supabase/supabase-js';
-
-const supabase = createClient(
-  "https://abmfjpolieweyzlwkjfw.supabase.co",
-  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImFibWZqcG9saWV3ZXl6bHdramZ3Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3MjkyNjQxNTcsImV4cCI6MjA0NDg0MDE1N30.g5IxVog2K6iA2abr0VzM-_2YCZjJnGvEoRqIj7tU5XU"
-);
-
-export { supabase as s };
